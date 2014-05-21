422f667e69f383626117f3a509ecd09ef1abb82c
Maksim <max@koritskiy.ru>
2014-05-21 16:03:32 +0400

    add mobile support

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require ./colResizable-1.3.min.js
-//= require ./jquery.turbolinks
+//= require ./slidebars
 //= require ./mobile
+//= require ./jquery.turbolinks
 //= require ./turbolinks
 //= require ./select2
 //= require ./select2_locale_ru.js
